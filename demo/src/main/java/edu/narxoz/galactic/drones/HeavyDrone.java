dbad6c4721c418a63768b5db0a2ed153e8b98274
aidosonalbayev-arch <aidos.onalbayev@narxoz.kz>
2026-01-28 23:23:36 +0500

    implement light and heavy drones

--- demo/src/main/java/edu/narxoz/galactic/drones/HeavyDrone.java
+++ demo/src/main/java/edu/narxoz/galactic/drones/HeavyDrone.java
@@ -1,5 +1,11 @@
 package edu.narxoz.galactic.drones;
 
-public class HeavyDrone {
-    
+public class HeavyDrone extends Drone{
+    public HeavyDrone(String id, double maxPayloadKg) {
+        super(id, maxPayloadKg);
+    }
+    @Override
+    public double speedKmPerMin(){
+        return  5.0;
+    }
 }
--- demo/src/main/java/edu/narxoz/galactic/drones/LightDrone.java
+++ demo/src/main/java/edu/narxoz/galactic/drones/LightDrone.java
@@ -1,5 +1,14 @@
 package edu.narxoz.galactic.drones;
 
-public class LightDrone {
+public class LightDrone extends Drone {
+
+    public LightDrone(String id, double maxPayloadKg) {
+        super(id, maxPayloadKg);
+    }
+
+    @Override
+    public double speedKmPerMin(){
+        return 10.0;
+    }
     
 }
