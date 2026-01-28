03f7be1a9cc18875232d331b915ae2104cbaea52
aidosonalbayev-arch <aidos.onalbayev@narxoz.kz>
2026-01-28 23:23:08 +0500

    add base drone abstraction and status enum

--- demo/src/main/java/edu/narxoz/galactic/drones/Drone.java
+++ demo/src/main/java/edu/narxoz/galactic/drones/Drone.java
@@ -1,5 +1,35 @@
 package edu.narxoz.galactic.drones;
 
 public abstract class Drone {
+    private String id;
+    private DroneStatus status;
+    private double maxPayloadKg;
+
+    protected Drone(String id, double maxPayloadKg) {
+        if(maxPayloadKg<=0){
+            throw new IllegalArgumentException("Max payload must be greater than zero.");
+        }
+        this.id = id;
+        this.maxPayloadKg = maxPayloadKg;
+        this.status = DroneStatus.IDLE;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public DroneStatus getStatus() {
+        return status;
+    }
     
+    public double getMaxPayloadKg() {
+        return this.maxPayloadKg;
+    }
+
+    
+    public void setStatus(DroneStatus status) {
+        this.status = status;
+    }
+
+    public abstract double speedKmPerMin();
 }
