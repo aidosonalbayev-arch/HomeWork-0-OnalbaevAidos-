e5369241e3f570664a25c2ba3c41784472ccf52d
aidosonalbayev-arch <aidos.onalbayev@narxoz.kz>
2026-01-28 23:25:45 +0500

    add delivery task state machine

--- demo/src/main/java/edu/narxoz/galactic/task/DeliveryTask.java
+++ demo/src/main/java/edu/narxoz/galactic/task/DeliveryTask.java
@@ -1,5 +1,57 @@
 package edu.narxoz.galactic.task;
 
+import edu.narxoz.galactic.bodies.CelestialBody;
+import edu.narxoz.galactic.cargo.Cargo;
+import edu.narxoz.galactic.drones.Drone;
+
 public class DeliveryTask {
-    
+    private CelestialBody origin;
+    private CelestialBody destination;
+    private Cargo cargo;
+    private TaskState state;
+    private Drone assignedDrone;
+
+    public DeliveryTask(CelestialBody origin, CelestialBody destination, Cargo cargo) {
+        this.origin = origin;
+        this.destination = destination;
+        this.cargo = cargo;
+        this.state = TaskState.CREATED;
+        this.assignedDrone = null;
+    }
+
+    public CelestialBody getOrigin() {
+        return origin;
+    }
+    public CelestialBody getDestination() {
+        return destination;
+    }
+    public Cargo getCargo() {
+        return cargo;
+    }
+    public TaskState getState() {
+        return state;
+    }
+    public Drone getAssignedDrone() {
+        return assignedDrone;
+    }
+
+    public double estimateTime(){
+        double distance = origin.distanceTo(destination);
+        double speed = assignedDrone.speedKmPerMin();
+        if(assignedDrone == null){
+            throw new IllegalStateException("No drone assigned to the task.");
+        }
+        if(assignedDrone.speedKmPerMin() <= 0){
+            throw new IllegalArgumentException("Drone speed must be greater than zero.");  
+        }
+        return distance / speed;
+    }
+
+    public void setState(TaskState state) {
+        this.state = state;
+    }
+
+    public void setAssignedDrone(Drone drone) {
+        this.assignedDrone = drone;
+    }
 }
